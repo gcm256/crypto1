b1bcbcab1939ecad69917ef405d2c5313da5de5c
gcm <kashparida@Kashs-MacBook-Pro.local>
2020-01-14 09:39:07 -0800

    Correction in author.

--- DataCrypto.java
+++ DataCrypto.java
@@ -6,8 +6,8 @@ import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.GCMParameterSpec;
 
 class Main {
 
