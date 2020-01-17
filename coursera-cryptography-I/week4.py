f7c1dfb60932ae956a2d142ed92a2d2108293f2b
gcm <59884013+gcm256@users.noreply.github.com>
2020-01-17 08:54:34 -0800

    Add week4.py, works till last block.

---
+++ coursera-cryptography-I/week4.py
@@ -0,0 +1,69 @@
+import urllib2
+import sys
+import binascii
+import string
+from Crypto.Util.strxor import strxor
+
+TARGET = 'http://crypto-class.appspot.com/po?er='
+#--------------------------------------------------------------
+# padding oracle
+#--------------------------------------------------------------
+class PaddingOracle(object):
+    def query(self, q):
+        target = TARGET + urllib2.quote(q)    # Create query URL
+        req = urllib2.Request(target)         # Send HTTP request to server
+        try:
+            f = urllib2.urlopen(req)          # Wait for response
+        except urllib2.HTTPError, e:
+            #print "We got: %d" % e.code       # Print response code
+            if e.code == 404:
+                return True # good padding
+            return False # bad padding
+
+
+po = PaddingOracle()
+
+def main():
+    block_size = 16 #Bytes
+    ciphertext_hex_str = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4"
+    cipher = binascii.unhexlify(ciphertext_hex_str)
+    num_blocks = len(cipher)/block_size
+    ct_blocks = [cipher[i:i+block_size] for i in xrange(0, len(cipher), block_size)]
+    pt_blocks = [binascii.unhexlify("00")*block_size for i in xrange(0,num_blocks)]
+    d = dict(zip(range(len(ct_blocks)), ct_blocks))
+
+    for i in range(0,num_blocks-1):
+        curr_ct_blocks, m = ct_blocks[:i+2], pt_blocks[i]
+        for pos in range(0,block_size):
+            print "m = ", binascii.hexlify(m)
+            m = update_m_with_byte_at(pos, curr_ct_blocks, m)
+        print "M = ", m
+
+    print len(cipher), len(ciphertext_hex_str), num_blocks, len(ct_blocks)
+    print [binascii.hexlify(ct_blocks[i]) for i in [0,1,2,3]]
+    print d
+    print pt_blocks
+
+
+def update_m_with_byte_at(pos_from_end_of_m, curr_ct_blocks, m):
+    ls = 'The Magic Words are Squeamish Ossifrage' #string.printable
+    #print curr_ct_blocks
+    ct_prefix, ct_to_change, ct_end = curr_ct_blocks[:-2], curr_ct_blocks[-2], curr_ct_blocks[-1]
+    k = pos_from_end_of_m + 1
+    n = len(m)
+    pad_gen_block = binascii.unhexlify("00"*(n-k) + ("%0.2x" % k)*k)
+    print "Pad = ", binascii.hexlify(pad_gen_block)
+    for g in set(ls):
+        m = m[:n-k] + g + m[n-k+1:]
+        print "PPP = ", m
+        #print len(ct_to_change), len(m), len(pad_gen_block)
+        ct_changed = strxor(ct_to_change, strxor(m,pad_gen_block))
+        query_str = binascii.hexlify("".join(ct_prefix) + ct_changed + ct_end)
+        print "Trying ", g, "Query = ", len(query_str)/2, " bytes, ", query_str
+        if po.query(query_str):
+            print g, "Works!"
+            return m
+
+
+if __name__ == "__main__":
+    main()
